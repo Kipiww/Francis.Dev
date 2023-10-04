e0e27580d334a09715b8d7941eaa93a2a3336b5b
Kipiww <130513312+Kipiww@users.noreply.github.com>
2023-10-04 16:40:34 +0800

    commit

--- script.js
+++ script.js
@@ -10,8 +10,6 @@ const sections = document.querySelectorAll('section');
 
 //func to scroll to view
 const clickScroll = (sec) => {sec.scrollIntoView({behaviour : 'smooth'});}
-//callbacks
-    
     aboutBtn.addEventListener('click', () => {clickScroll(sections[0]);});
     projectBtn.addEventListener('click',() => {clickScroll(sections[1]);});
     contactBtn.addEventListener('click',() => {clickScroll(sections[2]);});
@@ -23,19 +21,32 @@ clicktoScroll();
 const toggleDisplay = () => {
     const toggleBtn = document.getElementById('toggle');
     const body = document.querySelector('body');
-    const i = document.querySelector('i');
+    const icon = document.querySelector('i');
     const span = document.querySelectorAll('span');
     const a = document.querySelectorAll('a');
+    const p = document.querySelectorAll('p');
 
     //toggle DarkMode
     toggleBtn.addEventListener('click', () => {
+        //dark mode
+        icon.classList.toggle('active');
+        if (icon.classList.contains('active')) {
+            icon.classList.remove('bi-moon');
+            icon.classList.add('bi-sun');
+        } else {
+            icon.classList.remove('bi-sun');
+            icon.classList.add('bi-moon');
+        }
+        //DOMS
         body.classList.toggle('dark-mode');
-        span[0].classList.toggle('span-dark');
-
+       span.forEach((spans) => {
+        spans.classList.toggle('span-dark');
+       })
         const navLinks = Array.from(a).slice(0,5)  
-        a.forEach((element) => [
+        navLinks.forEach((element) => [
             element.classList.toggle('hover-color')
         ])
+        
     })
 };
 toggleDisplay();
