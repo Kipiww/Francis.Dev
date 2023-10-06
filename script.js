b0c70a728e18685f95d8d4ada7660e0cf3874e18
Kipiww <130513312+Kipiww@users.noreply.github.com>
2023-10-06 17:46:32 +0800

    commit

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 //Function to click navbar to scroll
 const clicktoScroll = () => {
 //buttons
-const homeBtn = document.getElementById('home');
 const aboutBtn = document.getElementById('about');
 const projectBtn = document.getElementById('projects');
 const contactBtn = document.getElementById('contact');
@@ -25,29 +24,32 @@ const toggleDisplay = () => {
     const span = document.querySelectorAll('span');
     const a = document.querySelectorAll('a');
     const p = document.querySelectorAll('p');
+    const img = document.querySelectorAll('img');
 
     //toggle DarkMode
     toggleBtn.addEventListener('click', () => {
         //dark mode
         icon.classList.toggle('active');
         if (icon.classList.contains('active')) {
-            icon.classList.remove('bi-moon');
-            icon.classList.add('bi-sun');
-        } else {
-            icon.classList.remove('bi-sun');
-            icon.classList.add('bi-moon');
+         icon.classList.remove('bi-moon');
+         icon.classList.add('bi-sun');
+        } 
+        else {
+         icon.classList.remove('bi-sun');
+         icon.classList.add('bi-moon');
         }
         //DOMS
         body.classList.toggle('dark-mode');
-       span.forEach((spans) => {
-        spans.classList.toggle('span-dark');
-       })
-        const navLinks = Array.from(a).slice(0,5)  
-        navLinks.forEach((element) => [
-            element.classList.toggle('hover-color')
-        ])
-        
-    })
-};
-toggleDisplay();
+        span.forEach((spans) => {spans.classList.toggle('span-dark');})
+       //navs
+        const navLinks = Array.from(a).slice(0,5);
+        navLinks.forEach((element) => {element.classList.toggle('hover-color')})
+        //download cv
+        a[5].classList.toggle('cv');
+        a[6].classList.toggle('cv');
 
+        //dark mode about me
+
+    });
+}
+toggleDisplay();
